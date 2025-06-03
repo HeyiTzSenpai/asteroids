9cfba6bb746aeb4b8bdf2a12412ded03ba3553a1
HeyiTzSenpai <qazwsx1435@hotmail.com>
2025-06-03 14:34:40 -0400

    A: Initial pygame setup with game loop

---
+++ main.py
@@ -0,0 +1,30 @@
+import pygame
+from constants import *
+
+
+def main():
+    # initialize all imported pygame modules
+    pygame.init()
+
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    print("Starting Asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+
+    while True:
+        #This will check if the user has closed the window and exit the game loop if they do.
+        #It will make the window's close button work.
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill((0, 0, 0))
+
+        #method to refresh the screen.
+        pygame.display.flip()
+
+
+# This line ensures the main() function is only called when this file is run directly;
+# it won't run if it's imported as a module.
+
+if __name__ == "__main__":
+    main()
