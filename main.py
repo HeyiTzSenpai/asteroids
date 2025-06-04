98a4d316e91ef353d300e27354cf0f8fa546b694
HeyiTzSenpai <qazwsx1435@hotmail.com>
2025-06-04 17:49:12 -0400

    F: add asteroid objects (drawable and updatable) and collision detection between the player and the asteroids

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
+import sys
+
 import pygame
 
+from asteroid import Asteroid
 from constants import *
 from player import Player
+from asteroidfield import *
 
 
 def main():
@@ -14,9 +18,18 @@ def main():
     # represent the amount of time that has passed since the last frame was drawn.
     dt = 0
 
+    asteroids = pygame.sprite.Group()
+    updatable_group = pygame.sprite.Group()
+    drawable_group = pygame.sprite.Group()
+    Player.containers = (updatable_group, drawable_group)
+    Asteroid.containers = (asteroids, updatable_group, drawable_group)
+    AsteroidField.containers = updatable_group
+
     x = SCREEN_WIDTH / 2
     y = SCREEN_WIDTH / 2
     player = Player(x, y)
+
+    asteroid = AsteroidField()
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     print("Starting Asteroids!")
@@ -31,9 +44,15 @@ def main():
                 return
         screen.fill((0, 0, 0))
 
-        player.draw(screen)
+        updatable_group.update(dt)
 
-        player.update(dt)
+        for asteroid in asteroids:
+            if asteroid.colliding(player):
+                print("Game over!")
+                sys.exit(0)
+
+        for drawable in drawable_group:
+            drawable.draw(screen)
 
         # method to refresh the screen.
         pygame.display.flip()
